c417c9429d6f5a047bce779a16f2c6f5632f0cb8
Joshua <pointtech1924@gmail.com>
2024-04-14 00:12:31 +0530

    remove

--- weather3/op.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = 'api_key'
-
-user_input = input("Enter city: ")
-
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
-print(weather_data.json())
